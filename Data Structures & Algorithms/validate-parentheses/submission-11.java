67ed1bc809122515d37f606a95f0fbc365ed0604
Dion Kadriu <77997015+DionKadriu@users.noreply.github.com>
2026-09-18 10:10:03 +0200

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+      Stack<Character> stack = new Stack<>();
+
+      for (char c : s.toCharArray()){
+
+        if (c == ')' || c == ']' || c == '}'){
+            if (stack.isEmpty()){
+                return false;
+            }
+            if (c == ')' && stack.pop() != '('){
+                return false;
+            }
+            else if ( c == ']' && stack.pop() != '['){
+                return false;
+            }
+            else if ( c == '}' && stack.pop() != '{'){
+                return false;
+            }
+        }
+        else{
+            stack.add(c);
+        }
+
+      }  
+      return stack.isEmpty();
+    }
+}
